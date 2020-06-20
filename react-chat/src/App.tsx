729762f5a946de6e6db6eec0228caf44ba1494b2
tinyperson4097 <feefe2344@gmail.com>
2020-06-20 15:32:41 -0400

    react chat

---
+++ react-chat/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+import { Chatbox } from './components/Chatbox';
+import { Dashboard } from './components/Dashboard';
+
+const App = () => (
+  <div className="App">
+    <Dashboard />
+  </div>
+);
+
+export default App;
